903007817b68f6d1543ddb28803fedbccf78896a
RayLoon <85132372+RayLoon@users.noreply.github.com>
2021-06-02 07:08:02 +0800

    First Commit

---
+++ LoonProject/urls.py
@@ -0,0 +1,81 @@
+
+
+from django.urls import path
+from django.conf.urls import url
+from MNList import views 
+#from updater import views
+
+from django.contrib import admin
+
+urlpatterns = [  
+    path("update_server/", views.update, name="update"),	  
+    url('admin/', admin.site.urls),
+    url(r'^$', views.MainPage, name='mainpage'),    
+    url(r'^MNList/new$', views.new_ibrgy, name='new_ibrgy'),    
+    url(r'^MNList/(\d+)/$', views.view_ibrgy, name='view_ibrgy'),    
+    url(r'^MNList/(\d+)/add_info$', views.add_info, name='add_info'),]
+    
+    
+    
+
+
+'''
+here the name of my app is updater so I add the import my view from there
+replace updater with your app-name where you have your views.py
+'''
+
+
+
+    
+
+'''
+
+urlpatterns = [    
+    url(r'^new$', views.new_list, name='new_list'),    
+    url(r'^(\d+)/$', views.view_list, name='view_list'),   
+    url(r'^(\d+)/add_item$', views.add_item, name='add_item'),]
+
+
+from django.conf.urls import include, url
+from MNList import views as list_views  
+from MNList import urls as list_urls  
+
+urlpatterns = [    
+    url(r'^$', views.MainPage, name='mainpage'), 
+    url(r'^MNList/', include(list_urls)),  ]
+urlpatterns = [    
+    url(r'^$', views.MainPage, name='mainpage'),    
+    url(r'^MNList/new$', views.new_list, name='new_list'),    
+    url(r'^MNList/(\d+)/$', views.view_list, name='view_list'),    
+    url(r'^MNList/(\d+)/add_item$', views.add_item, name='add_item'),]
+    
+urlpatterns = [    
+    url(r'^$', views.MainPage, name='mainpage'),    
+    url(r'^MNList/new$', views.new_list, name='new_list'),    
+    url(r'^MNList/(d.+)/$', views.view_list, name='view_list'),
+]    
+
+
+urlpatterns = [   
+    url(r'^$', views.MainPage, name='home'),       
+    url(r'^MNList/new$', views.new_list, name='new_list'),    
+    url(r'^MNList/the-only-list-in-the-world/$', views.view_list, name='view_list'),]
+
+
+'''
+"""RloonProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
---
+++ MNList/views.py
@@ -0,0 +1,121 @@
+#from django.http import HttpResponse    
+from django.shortcuts import redirect, render
+from MNList.models import BResidents, IBrgy, SBeneficiary, SDistributions, StatusDB
+#import git
+from django.views.decorators.csrf import csrf_exempt
+
+
+def MainPage(request): 
+     ibrgys = IBrgy.objects.all()
+     return render(request, 'BSMS.html',{'ibrgys' : ibrgys})
+     
+  
+def add_info(request, ibrgy_id):    
+   ibrgy_ = IBrgy.objects.get(id=ibrgy_id)    
+   BResidents.objects.create(rname=request.POST['addFM'],rrelation=request.POST['addRS'],radd=request.POST['addadd'], ibrgy=ibrgy_)
+   return redirect(f'/MNList/{ibrgy_.id}/')    
+   
+def view_ibrgy(request, ibrgy_id):    
+   ibrgy_ = IBrgy.objects.get(id=ibrgy_id)
+   return render(request, 'SInfo.html', {'ibrgy': ibrgy_})
+   
+def new_ibrgy(request):   
+    
+    newibrgy_ = IBrgy.objects.create(mncplty=request.POST['Municipality'],bname=request.POST['Brgy'],bID=request.POST['BrgyID'])
+    return redirect(f'/MNList/{newibrgy_.id}/') 
+  
+   #ibrgy_ = IBrgy.objects.create()
+   #Info.objects.create(ibrgy=ibrgy_)
+   #return redirect(f'/MNList/{ibrgy_.id}/')
+    
+   #return redirect('/MNList/the-only-list-in-the-world/')
+   #Info.objects.create(text=request.POST['itext'])
+   #return redirect('/MNList/the-only-list-in-the-world/')
+   #return redirect('/MNList/the-only-list-in-the-world/')
+
+def dtmanipulation(request):
+   
+    #Creating a data
+    ibrgy= IBrgy(mncplty="Dasmariñas City", bname="Brgy. Victoria", bID="301185")
+    ibrgy.save()
+    
+    #Read All data
+    ibrgy = IBrgy.objects.all()
+    result = 'Printing all barangay entries in model: <br>'
+    for x in objects:
+        res += x.bname+"<br>"
+        
+    #Read a specific data    
+    ibrgy = IBrgy.objects.get(id="Brgy")   
+    res += 'Printing One entry <br>'
+    res += ibrgy.Brgy
+      
+    #Delete a data
+    res += '<br> Deleting an entry <br>'
+    ibrgy.delete()
+    
+    bresidents = BResidents.objects.get(rname = 'Sasuke Uchiha')
+    ibrgy.radd = "B 7 L 7"
+    ibrgy.save()
+    res = ""
+    
+    #Filtering data:
+    qs = BResidents.objects.filter(rname = "Sasuke Uchiha")
+    res += "Found: %s results<br>"%len(qs)
+    
+    #ordering results
+    qs = IBrgy.objects.order_by("mncplty")
+    for x in qs:
+        res += x.bname + x.bID +'<br>'
+        
+        
+@csrf_exempt
+def update(request):
+    if request.method == "POST":
+        '''
+        pass the path of the diectory where your project will be 
+        stored on PythonAnywhere in the git.Repo() as parameter.
+        Here the name of my directory is "test.pythonanywhere.com"
+        '''
+        repo = git.Repo("test.pythonanywhere.com/") 
+        origin = repo.remotes.origin
+
+        origin.pull()
+
+        return HttpResponse("Updated code on PythonAnywhere")
+    else:
+        return HttpResponse("Couldn't update the code on PythonAnywhere")       
+        
+        
+        
+        
+        
+'''      
+
+def MainPage(request): 
+   if request.method == 'POST':    
+     #new_itext = request.POST['itext']  
+     Info.objects.create(text=request.POST['itext'])
+     #Info.objects.create(text=new_itext)  
+     return redirect('/MNList/the-only-list-in-the-world/')
+     
+  #items = Item.objects.all()
+  #return render(request, 'mynotes.html', {'items': items}) 
+   return render(request, 'mynotes.html') 
+   
+def MainPage(request): 
+   item = Item()   
+   item.text = request.POST.get('item_text', '')    
+   item.save()
+   
+   return render(request, 'mynotes.html', { 
+        'new_item_text': item.text
+   })
+   
+   else:       
+     new_itext = '' 
+   return render(request, 'mynotes.html', {        
+    'new_itext': new_itext,     
+})
+   
+'''
